74d174bdef1b8dedf4eb534ee33938c64b9facaf
Tompatterson9 <thomas.patterson99@gmail.com>
2021-03-24 16:18:21 +0000

    changed font of text area

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,17 +1,21 @@
 import styled from "styled-components";
-
-
+import '../styles/index.css';
 
 function Note() {
-    const Text = styled.textarea`
-        background-color:#28112B;
-        border:none;
-        width:100%;
-        color:white;
-        margin:20px;
+  const Text = styled.textarea`
+    background-color: #28112b;
+    width: 95vw;
+    color: white;
+    height: 90vh;
+    margin: 20px;
+    resize: none;
+    border-style: none;
+    border-color: Transparent;
+    overflow: auto;
+    border:0px;
+    font-family: 'Roboto', sans-serif;
+  `;
 
-    `
-    
   return (
     <>
       <Text id="area" rows="10" cols="50"></Text>
