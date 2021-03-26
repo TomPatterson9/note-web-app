d590585e3c98913d2d923d315063cca7abdaea93
Tompatterson9 <thomas.patterson99@gmail.com>
2021-03-26 00:11:21 +0000

    styled the form

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,12 +2,30 @@ import styled from "styled-components";
 
 function Login(){
 
+    const LoginCard = styled.div `
+
+        width:50vw;
+        height:40vh;
+        position:absolute; 
+    
+    `
+    const InputField = styled.input ` 
+
+        width:10vw;
+        height: 4vh;
+        border-radius:4px;
+        margin:5px;
+        text-align:center; 
+    
+    `
+
     return (
         <div>
-            <form>
-              <input type="text" placeholder="Email" />
-              <input type="password" placeholder="Password" />
-            </form>
+            <LoginCard>
+              <InputField type="text" placeholder="Email" />
+              <br />
+              <InputField type="password" placeholder="Password" />
+            </LoginCard>
         </div>
     );
 }
