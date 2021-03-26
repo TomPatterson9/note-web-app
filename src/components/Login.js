ec6ccc91d46baf76434e5d6a9e0230f5b45e208c
Tompatterson9 <thomas.patterson99@gmail.com>
2021-03-26 15:02:01 +0000

    styled login form and added login button

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,32 +1,47 @@
 import styled from "styled-components";
 
-function Login(){
+function Login() {
+  const LoginCard = styled.div`
+    width: 30vw;
+    height: 50vh;
+    position: absolute;
+    left: 35vw;
+    top: 30vh;
+    display: flex;
+    background-color: #220c10;
+    flex-wrap: wrap;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    min-width:270px;
+    min-height:60px;
+  `;
+  const InputField = styled.input`
+    width: 250px;
+    height: 40px;
+    border-radius: 4px;
+    text-align: center;
+    margin: 10px;
+  `;
 
-    const LoginCard = styled.div `
+  const LoginButton = styled.button`
+    width: 250px;
+    height: 40px;
+    border-radius: 4px;
+    text-align: center;
+    margin: 10px; 
+  `;
 
-        width:50vw;
-        height:40vh;
-        position:absolute; 
-    
-    `
-    const InputField = styled.input ` 
-
-        width:10vw;
-        height: 4vh;
-        border-radius:4px;
-        margin:5px;
-        text-align:center; 
-    
-    `
-
-    return (
-        <div>
-            <LoginCard>
-              <InputField type="text" placeholder="Email" />
-              <br />
-              <InputField type="password" placeholder="Password" />
-            </LoginCard>
-        </div>
-    );
+  return (
+    <div>
+      <LoginCard>
+        <InputField type="text" placeholder="Email" />
+        <br />
+        <br />
+        <InputField type="password" placeholder="Password" />
+        <LoginButton>Login</LoginButton>
+      </LoginCard>
+    </div>
+  );
 }
 export default Login;
